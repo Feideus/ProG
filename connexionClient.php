6becfb39a15596249b69d1d40d095e66eb210269
Feideus <erwan.ulrich@gmail .com>
2016-04-28 19:05:02 +0300

    LE JEUDI SOIR

--- connexionClient.php
+++ connexionClient.php
@@ -10,25 +10,24 @@ catch (Exception $e)
         die('Error 1.2.3.4.Z connection a la base');
 }
 
-$req = $bdd->prepare('SELECT id FROM Touitos WHERE pseudonyme = :id AND mdp = :mdp');
+$req = $bdd->prepare('SELECT id FROM Touitos WHERE pseudonyme = :id AND motPasse = :mdp');
 
 
 $req->execute(array(
     'id' => $_POST['pseudonyme'],
-    'mdp' => $_POST['$mdp']));
+    'mdp' => $_POST['mdp']));
 
 
-
-if(!isset($req))
+if($req->fetch() === false)
 {
-    echo "la connexion a echouee";
+    require("connexion.php");
 }
 
 else
 {
+    require("wall.html");
     $resultat['pseudonyme'] = $_POST['pseudonyme'];
-    session_start();
     $_SESSION['pseudo'] = $resultat['pseudonyme'];
-    echo "bienvenue".$resultat['pseudonyme']."Touitteur est heureux de vous revoir !";
-}
+}    
 ?>
+
--- inscription.php
+++ inscription.php
@@ -1,80 +1,97 @@
-<html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/php; charset=utf-8"/>
-		<meta name="LANG" content="fr"/>
-		<meta name="DESCRIPTION" content=" Page d'inscription"/>
-      <title> Page d'inscription</title>	
-	</head>
-	<body>
-		<article>
-			<?php
-				function testchamps($pseudo,$passe,$passe2,$email){
-						if(!empty($pseudo)){// D'abord,je teste si le champs login est non vide.  
-							if(!empty($passe)&&!empty($passe2)){// Ensuite, je teste si le champs mdp est non vide.
-								if($passe === $passe2){	
-									if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-											return true;
-									}
-								}	
+<?php
+		 /**
+		 *
+		 */
+ class inscription 
+ {
+		 	var $pseudo;
+		 	var $passe;
+		 	var $passe2;
+		 	var $email;
+
+			function testchamps()
+            {
+				if(!empty(this_$pseudo))
+                {// D'abord,je teste si le champs login est non vide.  
+					if(!empty(this_$passe)&&!empty(this_$passe2))
+                    {// Ensuite, je teste si le champs mdp est non vide.
+						if(this$passe === this$passe2)
+                        {	
+							if (filter_var(this$email, FILTER_VALIDATE_EMAIL)) 
+                            {
+									return true;
 							}
-						}
-						return false;
+						}	
+					}
 				}
-				function ajout($pseudo,$email,$passe){			
-								$bdd = new PDO('mysql:host=localhost;dbname=BDE11200183', 'E11200183', '2405067885Y');		
+				return false;
+			}
+         
+
+function ajoutTouitos($id,$pseudo,$email,$mdp,$t,$status)
+			{			
+								$bdd = new PDO('mysql:host=localhost;dbname=TOUITTEUR', 'Feideus', 'lebossdesboss32');		
 								// Je me connecte à la base de données .		
-								// Je vais crypter le mot de passe.
-								$passecoder = md5($passe);	
-								$bdd->query("INSERT INTO Touitos VALUES('','$pseudo','$email','$passecoder','','')");
+								// Je vais crypter le mot de passe.	
+								$bdd->prepare("INSERT INTO Touitos VALUES(:id,:pseudonyme,:email,:mdp,:t,:status)");
+                                $bdd->execute(array('id' => $id, 'pseudonyme' => $pseudo, 'email' => $email, 'mdp' => $mdp, 't' => $t , 'status' => $status);
 								
-				}
-				function TestTouitos ($login, $email)
-				{
-						$bdd = new PDO('mysql:host=localhost;dbname=BDE11200183', 'E11200183', '2405067885Y');		
-						// Je me connecte à la base de données .
-						$Touitos = $bdd ->prepare("SELECT pseudonyme, email from Touitos");
+			}
 
-						$Touitos->execute();
+            function TestTouitos ($ARGlogin,$ARGemail)
+			{
+				var $login;
+                var $email;
 
-						$Touitos->setFetchMode(PDO::FETCH_OBJ);
+                $login = $ARGlogin;
+                $email = $ARGemail;
 
-						while($user = $Touitos->fetch()){
-							if (strcmp($user->pseudonyme, $login)){
-								if(strcmp($user->email,$email)){
-									return 2;
-								}
-								else{
-									return 3;
-								}
+				$bdd = new PDO('mysql:host=localhost;dbname=TOUITTEUR', 'Feideus', 'lebossdesboss32');		
+				// Je me connecte à la base de données .
+				$Touitos = $bdd ->prepare("SELECT pseudonyme, email from Touitos");
+
+				$Touitos->execute();
+				$Touitos->setFetchMode(PDO::FETCH_OBJ);
+
+				while($user = $Touitos->fetch()){
+					if (strcmp($user->pseudonyme, $ARGlogin))
+					{
+						if(strcmp($user->email,$ARGemail))
+						{
+							return 2;
+						}
+							else
+							{
+								return 3;
 							}
 						}
-						return 1;
-				}
-				$passe = htmlentities($_POST['mdp']);
-				$passe2 = htmlentities($_POST['mdp2']);
-				$pseudo = htmlentities($_POST['pseudo']);
-				$email = htmlentities($_POST['email']);
-				if(testchamps($pseudo,$passe,$passe2,$email)){
-					switch (TestTouitos ($pseudo, $email)){
+					}
+					return 1;
+		   }
+                                
+                $this_passe = htmlentities($_POST['mdp']);
+				$this_passe2 = htmlentities($_POST['mdp2']);
+				$this_pseudo = htmlentities($_POST['pseudo']);
+				$this_email = htmlentities($_POST['email']);
+				if(testchamps()){
+					switch (TestTouitos($this_pseudo, $this_email)){
 						case 1 :
-							ajout($pseudo,$email,$passe);
+							ajout($this_pseudo,$this_email,$this_passe);
 							echo '<p>Compte crée!<br/> Voivi le lien vers la page d\'index<br/> <a href="connexion.html">Se connecter</a></p>';
 							break;
 						case 2 :
-							header('Location: http://marseille/~11200183/Touitteur/inscription.html'); 
+							require("inscription.php"); 
 							exit();
 							echo "pseudo deja utilisé"; 
 							break;
 						case 3 :
-							header('Location: http://marseille/~11200183/Touitteur/inscription.html'); 
+							require("inscription.php"); 
 							exit();
 							echo "email deja utilisé"; 
 							break;
 					}
-				}
-					
-			?>
-			
-		
-	</body>
-</html>
+
+                }
+            }
+ }
+?>
