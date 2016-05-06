e2ec3ba9c92f09157074e5aaecb5151d47d4c0f6
Feideus <erwan.ulrich@gmail.com>
2016-05-06 17:12:54 +0200

    HOLOLO

---
+++ AfficherMessages.php
@@ -0,0 +1,47 @@
+<?php
+
+$LeNom = $_POST["LeNomDest"];
+$NameHost = $_POST["NameHost"];
+
+
+if ($LeNom !== "") 
+{
+    $LeNom = strtolower($LeNom);
+}
+
+try
+{
+    $bdd=new PDO('mysql:host=localhost;dbname=Touiteur','erwan','lebossdesboss32');
+}
+
+catch (Exception $e)
+{
+        die('Error 1.2.3.4.Z connection a la base');
+}
+$req = $bdd->prepare('SELECT id FROM Touitos WHERE pseudonyme = :pseudonyme');
+$req3 = $bdd->prepare('SELECT id FROM Touitos WHERE pseudonyme = :pseudonyme');
+$req2 = $bdd->prepare('SELECT texte,IDAuteur FROM Touites,TouitesPrives WHERE
+(Touites.IDMsg = TouitesPrives.IDMsg
+AND
+((TouitesPrives.IDAuteur = :idAuteur AND TouitesPrives.IDReceveur = :idreceveur)
+OR
+(TouitesPrives.IDReceveur = :idAuteur AND TouitesPrives.IDAuteur = :idreceveur)))');
+
+$req3->execute(array('pseudonyme' => $NameHost));
+$req->execute(array('pseudonyme' => $LeNom));
+
+$IDDest = $req->fetch();
+$IDAut2 = $req3->fetch();
+
+
+$req2->execute(array('idAuteur' => $IDDest['id'], 'idreceveur' => $IDAut2['id']));
+
+
+while($Touites = $req2->fetch())        
+{
+    $req4 = $bdd->prepare('SELECT pseudonyme FROM Touitos WHERE ID = :idAuteur');
+    $req4->execute(array('idAuteur' => $Touites['IDAuteur']));
+    $tmp = $req4->fetch();
+	echo " message de ".$tmp['pseudonyme']." : ".$Touites['texte']."<br/>";
+}
+?>
---
+++ makecookie.js
@@ -0,0 +1,7 @@
+function Cookpseudo(pseudo)
+{
+	var pseudonyme = document.getElementById("pseudonyme");
+	var name = pseudonyme.value;
+	document.body.style.backgroundColor = name;
+	$.cookie("name",pseudonyme.value);
+}
